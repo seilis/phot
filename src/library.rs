f025a344bd6d2bc0731a37ad25da5bdf6407be2f
Aaron Seilis <aaron.seilis@seilis.ca>
2021-02-15 11:46:01 -0800

    Add CLI arguments
    Add some basic CLI arguments with `clap`; currently has `new`
    and `add`.
    
    - `new` ensures that there's a library present.
    - `add` adds a file to the library. Currently copies the file
      verbatim.

--- src/library.rs
+++ src/library.rs
@@ -1,6 +1,8 @@
 use std::path::{Path, PathBuf};
 use std::fs;
+use std::io::Read;
 use thiserror::Error;
+use sha1::Sha1;
 
 const DEFAULT_LIB_PATH: &str = "phot";
 
@@ -11,6 +13,9 @@ pub enum LibError {
 
     #[error("io error")]
     IO(#[from] std::io::Error),
+
+    #[error("unsupported file type")]
+    UnsupportedFileType
 }
 
 type Result<T> = std::result::Result<T, LibError>;
@@ -44,6 +49,12 @@ impl Library {
         dir
     }
 
+    fn get_object_path(&self, hash: &str) -> PathBuf {
+        let mut path = self.get_objects_dir();
+        path.push(&hash[0..4]);
+        path
+    }
+
     pub fn create(&self) -> Result<()> {
         // Make the base path first
         fs::create_dir_all(self.base_path.as_path())?;
@@ -53,5 +64,32 @@ impl Library {
 
     pub fn get_path(&self) -> &Path {
         self.base_path.as_path()
+    }
+
+    pub fn add_file(&mut self, path: &Path) -> Result<()> {
+        let attrs = fs::metadata(path)?;
+
+        if !attrs.is_file() {
+            return Err(LibError::UnsupportedFileType);
+        }
+
+        let mut file = fs::File::open(path)?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)?;
+
+        // Calculate the sha1 sum for our datastore
+        let mut sum = Sha1::default();
+        sum.update(&buffer);
+
+        let sum_string = sum.digest().to_string();
+
+        let mut lib_path = self.get_object_path(&sum_string);
+
+        fs::create_dir_all(&lib_path)?;
+        lib_path.push(&sum_string);
+        fs::copy(path, lib_path)?;
+        println!("success! sum: {:?}", sum.digest().to_string());
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
+use clap::{Arg, App, SubCommand};
+use std::path::PathBuf;
 
 use phot::library::Library;
 
 fn main() -> Result<()> {
-    let lib = Library::new();
-    lib.create().expect("oops, that didn't work");
-    println!("Created a library at {}", lib.get_path().to_str().expect("no library path"));
+
+    let args = App::new("phot")
+                .version("0.1")
+                .author("Aaron Seilis <aaron.seilis@seilis.ca>")
+                .about("Photo library management tool")
+                .subcommand(
+                    SubCommand::with_name("new")
+                        .about("make a new library")
+                )
+                .subcommand(
+                    SubCommand::with_name("add")
+                        .about("add an image or group of images to the library")
+                        .arg(
+                            Arg::with_name("input")
+                                .help("the file or directory to add to the library")
+                                .index(1)
+                                .required(true),
+                            )
+                )
+                .get_matches();
+
+    match args.subcommand() {
+        ("new", Some(new_args)) => {
+            let lib = Library::new();
+            lib.create().with_context(|| format!("Failed to create a new library"))?;
+            println!("Created a library at {}", lib.get_path().to_str().expect("no library path"));
+        }
+        ("add", Some(add_args)) => {
+            let mut lib = Library::new();
+            let path = add_args.value_of("input").unwrap();
+            let path: PathBuf = path.into();
+            lib.add_file(path.as_path()).with_context(|| format!("Failed to add path {:?}", path))?;
+        }
+        _ => {
+            println!("no subcommand");
+        }
+    };
 
     Ok(())
 }
