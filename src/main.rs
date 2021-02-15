7874d825f105b5c5296cf9239337fbcfe445d9b9
Aaron Seilis <aaron.seilis@seilis.ca>
2021-02-15 11:37:40 -0800

    Add 'anyhow' library
    Add the 'anyhow' library to dependencies and make `main()` return
    an `anyhow::Result`. This will make error handling easier in future
    commits.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use anyhow::Result;
+
 use phot::library::Library;
 
-fn main() {
+fn main() -> Result<()> {
     let lib = Library::new();
     lib.create().expect("oops, that didn't work");
     println!("Created a library at {}", lib.get_path().to_str().expect("no library path"));
+
+    Ok(())
 }
